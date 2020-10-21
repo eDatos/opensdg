620b2633f4858204c4e9f59d05c4ba2561aefe93
José Carlos Rodríguez Cortés <jrodcor@arte-consultores.com>
2020-10-21 13:41:38 +0100

    EDATOS-3206 - Añadir al proyecto header y footer ISTAC
    > Resueltos los siguientes puntos:  El header y footer aparecen bien solamente en la página principal, pero una vez navegas a otra página ya no se ve., Mostrar por defecto en ES, Añadir barra blanca con el nombre de la aplicación los idiomas (Pendiente de Revisión en Estadísticas), Añadir diferentes idiomas (ES, EN), Navegación entre los tabs de los diferentes indicadores

---
+++ assets/js/setElement.js
@@ -0,0 +1,69 @@
+const HEADER = "HEADER";
+const FOOTER = "FOOTER";
+
+// Solo para demo local
+/*
+function setElement(elementType, elementId, configPath) {
+    fetch(configPath)
+        .then(res => res.json())
+        .then(json => {
+            fetchOptions = {
+                method: 'GET',
+                //mode: 'cors',
+                headers: {
+                    'Content-Type': 'text/html',
+                }
+            }
+
+            var element = null;
+            switch(elementType){
+                case HEADER:
+                    element = json.demo.header;
+                    break;
+                case FOOTER:
+                    element = json.demo.footer;
+                    break;
+            }
+            var htmlUrl = json.demo.endpoint + element + "?appName=" + json.metadata.appName;
+            fetch(htmlUrl, fetchOptions)
+                .then(res => res.text())
+                .then(html => {
+                    document.getElementById(elementId).innerHTML = html;
+                    
+                })
+        })
+}
+*/
+function setElement(elementType, elementId, configPath) {
+    fetch(configPath)
+        .then(res => res.json())
+        .then(json => {
+            var type = null
+            switch(elementType) {
+                case HEADER:
+                    type = json.metadata.navbarPathKey;
+                    break;
+                case FOOTER:
+                    type = json.metadata.footerPathKey;
+                    break;
+            }
+            var apiUrl = `${json.metadata.endpoint}/properties/${type}.json`;
+            fetch(apiUrl)
+                .then(res => res.json())
+                .then(htmlUrl => {
+                    fetchOptions = {
+                        method: 'GET',
+                        //mode: 'cors',
+                        headers: {
+                            'Content-Type': 'text/html',
+                        }
+                    }
+                    var appHtmlUrl = `${htmlUrl.value}?appName=${json.metadata.appName}`
+                    fetch(appHtmlUrl, fetchOptions)
+                        .then(res => res.text())
+                        .then(html => {
+                            document.getElementById(elementId).innerHTML = html;
+                        });
+                });
+        });
+}
