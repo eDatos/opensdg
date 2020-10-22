22aa4b71908287c62a10a32ca4762238fa6e4c50
José Carlos Rodríguez Cortés <jrodcor@arte-consultores.com>
2020-10-22 09:49:06 +0100

    EDATOS-3206 - Añadir al proyecto header y footer ISTAC
    > Resueltas las tareas: "Añadir Favicon", "Revisar estilos de barra azul", "Revisar estilos footer".

--- assets/js/setElement.js
+++ assets/js/setElement.js
@@ -1,69 +1,71 @@
 const HEADER = "HEADER";
 const FOOTER = "FOOTER";
+const DEMO = true // Para pasar rápido de demo a normal. TODO: BORRAR
 
-// Solo para demo local
-/*
-function setElement(elementType, elementId, configPath) {
-    fetch(configPath)
-        .then(res => res.json())
-        .then(json => {
-            fetchOptions = {
-                method: 'GET',
-                //mode: 'cors',
-                headers: {
-                    'Content-Type': 'text/html',
-                }
-            }
-
-            var element = null;
-            switch(elementType){
-                case HEADER:
-                    element = json.demo.header;
-                    break;
-                case FOOTER:
-                    element = json.demo.footer;
-                    break;
-            }
-            var htmlUrl = json.demo.endpoint + element + "?appName=" + json.metadata.appName;
-            fetch(htmlUrl, fetchOptions)
-                .then(res => res.text())
-                .then(html => {
-                    document.getElementById(elementId).innerHTML = html;
-                    
-                })
-        })
-}
-*/
-function setElement(elementType, elementId, configPath) {
-    fetch(configPath)
-        .then(res => res.json())
-        .then(json => {
-            var type = null
-            switch(elementType) {
-                case HEADER:
-                    type = json.metadata.navbarPathKey;
-                    break;
-                case FOOTER:
-                    type = json.metadata.footerPathKey;
-                    break;
-            }
-            var apiUrl = `${json.metadata.endpoint}/properties/${type}.json`;
-            fetch(apiUrl)
-                .then(res => res.json())
-                .then(htmlUrl => {
-                    fetchOptions = {
-                        method: 'GET',
-                        //mode: 'cors',
-                        headers: {
-                            'Content-Type': 'text/html',
-                        }
+if (DEMO) {
+    // Solo para demo local
+    function setElement(elementType, elementId, configPath) {
+        fetch(configPath)
+            .then(res => res.json())
+            .then(json => {
+                fetchOptions = {
+                    method: 'GET',
+                    //mode: 'cors',
+                    headers: {
+                        'Content-Type': 'text/html',
                     }
-                    var appHtmlUrl = `${htmlUrl.value}?appName=${json.metadata.appName}`
-                    fetch(appHtmlUrl, fetchOptions)
-                        .then(res => res.text())
-                        .then(html => {
-                            document.getElementById(elementId).innerHTML = html;
-                        });
-                });
-        });
+                }
+
+                var element = null;
+                switch (elementType) {
+                    case HEADER:
+                        element = json.demo.header;
+                        break;
+                    case FOOTER:
+                        element = json.demo.footer;
+                        break;
+                }
+                var htmlUrl = json.demo.endpoint + element + "?appName=" + json.metadata.appName;
+                fetch(htmlUrl, fetchOptions)
+                    .then(res => res.text())
+                    .then(html => {
+                        document.getElementById(elementId).innerHTML = html;
+
+                    })
+            })
+    }
+} else {
+    function setElement(elementType, elementId, configPath) {
+        fetch(configPath)
+            .then(res => res.json())
+            .then(json => {
+                var type = null
+                switch (elementType) {
+                    case HEADER:
+                        type = json.metadata.navbarPathKey;
+                        break;
+                    case FOOTER:
+                        type = json.metadata.footerPathKey;
+                        break;
+                }
+                var apiUrl = `${json.metadata.endpoint}/properties/${type}.json`;
+                fetch(apiUrl)
+                    .then(res => res.json())
+                    .then(htmlUrl => {
+                        fetchOptions = {
+                            method: 'GET',
+                            //mode: 'cors',
+                            headers: {
+                                'Content-Type': 'text/html',
+                            }
+                        }
+                        var appHtmlUrl = `${htmlUrl.value}?appName=${json.metadata.appName}`
+                        fetch(appHtmlUrl, fetchOptions)
+                            .then(res => res.text())
+                            .then(html => {
+                                document.getElementById(elementId).innerHTML = html;
+                            });
+                    });
+            });
+    }
 }
