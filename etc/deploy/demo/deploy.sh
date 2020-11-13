128a9660c46099e4ea714b1081848427ba9b6bb3
José Carlos Rodríguez Cortés <jrodcor@arte-consultores.com>
2020-11-13 12:23:43 +0000

    EDATOS-3216 - Automatización de la carga de datos
    > Corregido error por el que se movía un directorio que no necesitaba ser movido

--- etc/deploy/demo/deploy.sh
+++ etc/deploy/demo/deploy.sh
@@ -5,9 +5,10 @@ TRANSFER_PATH=$HOME_PATH/tmp
 ENV_CONF=$HOME_PATH/env
 DEPLOY_TARGET_PATH=/servers/estadisticas/opensdg
 OPENSDG_TAR_FILE=opensdg.tar.gz
+WEB_SRC_PATH=opensdg-web/src
 
 # scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r etc/deploy/config/demo/* deploy@estadisticas.arte.internal:$ENV_CONF
-scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r $OPENSDG_TAR_FILE deploy@estadisticas.arte.internal:$TRANSFER_PATH/$OPENSDG_TAR_FILE
+scp -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" -r $WEB_SRC_PATH/$OPENSDG_TAR_FILE deploy@estadisticas.arte.internal:$TRANSFER_PATH/$OPENSDG_TAR_FILE
 ssh -o ProxyCommand="ssh -W %h:%p deploy@estadisticas.arte-consultores.com" deploy@estadisticas.arte.internal <<EOF
 
     ###
