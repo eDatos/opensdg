7d62a13e35060822f920f95dc6ef7859e511423b
José Carlos Rodríguez Cortés <jrodcor@arte-consultores.com>
2020-12-15 13:08:09 +0000

    EDATOS-3222 - Revisión del contenido estático
    > Tapados botones de navegación cuando no se pasa un indicador siguiente/anterior
    > Modificado el build de OpenSDG para incluid únicamente las series en el archivo que contiene todos los indicaores para descarga
    > Añadido índice en la descarga de todos los indicadores

---
+++ opensdg-datos/src/scripts/__init__.py
@@ -0,0 +1,3 @@
+from . import fixes
+from . import build_data
+from . import check_data
--- opensdg-datos/src/scripts/build_data.py
+++ opensdg-datos/src/scripts/build_data.py
@@ -1,14 +1,20 @@
 # import shutil
-from sdg.open_sdg import open_sdg_build
-from sdg.open_sdg import open_sdg_check
+import os
+import re
+import fixes
+from sdg import open_sdg
+from sdg import IndicatorExportService
+
 
 # Validate the indicators.
 print("Validando datos...")
-validation_successful = open_sdg_check(config='config_data.yml')
+validation_successful = open_sdg.open_sdg_check(config='config_data.yml')
 
 # If everything was valid, perform the build.
 if not validation_successful:
     raise Exception('There were validation errors. See output above.')
 else:
+    print("Creando índice...")
+    fixes.create_index_csv()
     print("Construyendo datos...")
-    open_sdg_build(config='config_data.yml')
+    open_sdg.open_sdg_build(config='config_data.yml')
