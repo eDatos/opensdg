ccad549a10ce4e30c2218f08b34b24ec5d08b78f
Jorge González Cabrera <jgoncab@arte-consultores.com>
2022-10-17 13:45:10 +0100

    EDATOS-3757 - [ODS] Soporte multiidioma para la web de ODS
    > Reemplazando selector de idioma personalizado por el selector que proporciona el header de complementos-apps.

--- opensdg-web/src/assets/js/custom/setHeaderFooter.js
+++ opensdg-web/src/assets/js/custom/setHeaderFooter.js
@@ -2,24 +2,26 @@ const HEADER = "HEADER";
 const FOOTER = "FOOTER";
 
 function renderElement(elementId, htmlCode) {
-    document.getElementById(elementId).innerHTML = htmlCode;
+    var element = document.getElementById(elementId);
+    element.innerHTML = htmlCode;
+    reinsertScripts(element);
 }
 
-function cacheHtmlCode(elementId, htmlCode, language='') {
-    sessionStorage.setItem(elementId, htmlCode);
-    if (language !== '') {
-        sessionStorage.setItem(`${elementId}-${language}`, language);
+function reinsertScripts(el) {
+    const scriptList = el.getElementsByTagName('script');
+    for (const script of scriptList) {
+        const scriptCopy = document.createElement('script');
+        if (script.innerHTML) {
+            scriptCopy.innerHTML = script.innerHTML;
+        } else if (script.src) {
+            scriptCopy.src = script.src;
+        }
+        scriptCopy.async = false;
+        script.parentNode.replaceChild(scriptCopy, script);
     }
 }
 
-function removeCacheHtmlCode(elementId, language='') {
-    sessionStorage.removeItem(elementId);
-    if (language !== '') {
-        sessionStorage.removeItem(`${elementId}-${language}`);
-    }
-}
-
-function setElementRutine(elementType, elementId, configPath, baseUrl = '', appName = '', language = '') {
+function setElementRutine(elementType, elementId, configPath, baseUrl = '', appName = '', language = '', internationalizationParamId='') {
     fetch(configPath)
     .then(res => res.json())
     .then(json => {
@@ -42,25 +44,24 @@ function setElementRutine(elementType, elementId, configPath, baseUrl = '', appN
                         'Content-Type': 'text/html',
                     }
                 }
-                let appHtmlUrl = `${htmlUrl.value}`;
+                let appHtmlUrl = HEADER === elementType ? "http://localhost:8070/complementos-apps/organisations/istac/common/header/header.html" :`${htmlUrl.value}`; // cambio sólo para pruebas en local
                 appHtmlUrl += `?appName=${appName}`;
                 appHtmlUrl += `&appUrl=${baseUrl}`;
-                appHtmlUrl += `&lang=${language}`;
+                appHtmlUrl += language ? `&${internationalizationParamId}=${language}` : `&${internationalizationParamId}=${navigator.languages[0].toLowerCase()}`;
                 fetch(appHtmlUrl, fetchOptions)
                     .then(res => res.text())
                     .then(html => {
-                        cacheHtmlCode(elementId, html, language);
                         renderElement(elementId, html);
                     });
             });
     });
 }
 
-function setElement(elementType, elementId, configPath, baseUrl = '', appName='', language='') {
+function setElement(elementType, elementId, configPath, baseUrl = '', appName='', language='', internationalizationParamId='') {
     var cachedData = sessionStorage.getItem(elementId);
     if (cachedData != null) {
         renderElement(elementId, cachedData);
     } else {
-        setElementRutine(elementType, elementId, configPath, baseUrl, appName, language);
+        setElementRutine(elementType, elementId, configPath, baseUrl, appName, language, internationalizationParamId);
     }
 }
